2c88207d457489e834aede6afe21c4dbd0aa08c3
Evan Losin <evanlosin11@gmail.com>
2025-05-16 15:44:03 -0400

    added more functionality with favorited recipes

--- routes/recipes.js
+++ routes/recipes.js
@@ -20,12 +20,37 @@ router.post('/search', async (req, res) => {
 
 router.get('/favorites', async (req, res) => {
   const favorites = await Favorite.find().sort('-addedAt');
-  res.render('favorites', { favorites });
+  res.render('favorites', { favorites, title: 'My Favorite Recipes' });
 });
 
 router.post('/favorites', async (req, res) => {
   const { id, title, image } = req.body;
   await Favorite.create({ recipeId: id, title, image });
+  res.redirect('/favorites');
+});
+
+router.get('/favorites/:id', async (req, res) => {
+  const mongoId = req.params.id;
+  try {
+    // Find the favorite by MongoDB _id
+    const favorite = await Favorite.findById(mongoId);
+    if (!favorite) {
+      return res.status(404).send('Favorite not found');
+    }
+    // Use the recipeId from the favorite document
+    const response = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${favorite.recipeId}`);
+    const meal = response.data.meals ? response.data.meals[0] : null;
+    if (!meal) {
+      return res.status(404).send('Recipe not found');
+    }
+    res.render('favoriteDetail', { title: meal.strMeal, meal });
+  } catch (err) {
+    res.status(500).send('Error fetching recipe details');
+  }
+});
+
+router.post('/favorites/:id/remove', async (req, res) => {
+  await Favorite.findByIdAndDelete(req.params.id);
   res.redirect('/favorites');
 });
 
